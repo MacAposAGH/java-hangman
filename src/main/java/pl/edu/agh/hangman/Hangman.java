4d1f8ef543f9b6582fd38dcb24959726416c7ad7
MacAposAGH <mapostol@student.agh.edu.pl>
2026-01-31 12:12:07 +0100

    final

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -1,5 +1,9 @@
 package pl.edu.agh.hangman;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Hangman {
@@ -56,11 +60,58 @@ public class Hangman {
                     "========"
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         HangmanUI hangmanUI = new HangmanUI();
 
-        hangmanUI.Welcome();
-        hangmanUI.NextLetter();
+        FileReader fileReader = new FileReader();
+        ArrayList<String> strings = fileReader.scanFile();
+        String randomWord = RandomWordPicker.chooseRandomWord(strings).toLowerCase();
+
+        int counter = HANGMANPICS.length;
+        int i = 0;
+        int j = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        List<Character> list = toList(randomWord);
+        char[] chars = new char[randomWord.length()];
+        Arrays.fill(chars, '_');
+
+        while (true) {
+            System.out.println(chars);
+            char c = scanner.next().charAt(0);
+
+            int indexOf = list.indexOf(c);
+            if(indexOf >= 0 ){
+                list.set(indexOf, '_');
+                chars[indexOf] = c;
+                i++;
+            } else {
+                System.out.println(HANGMANPICS[j]);
+                counter--;
+                j++;
+            }
+
+            if(i==list.size()){
+                System.out.println("Wygrałeś :)");
+                break;
+            }
+
+            if (counter == 0) {
+                System.out.println("Przegrałeś :(");
+                break;
+            }
+        }
+
+        System.out.println(randomWord);
 
     }
+
+    public static List<Character> toList(String word) {
+        ArrayList<Character> strings = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            strings.add(word.charAt(i));
+        }
+        return strings;
+    }
+
 }
--- src/main/java/pl/edu/agh/hangman/RandomWordPicker.java
+++ src/main/java/pl/edu/agh/hangman/RandomWordPicker.java
@@ -7,6 +7,10 @@ import java.util.function.Predicate;
 public class RandomWordPicker {
     private static final Random RANDOM = new Random();
 
+    public static String chooseRandomWord(List<String> words) {
+        return words.get(RANDOM.nextInt(words.size()));
+    }
+
     public static String chooseRandomWord(List<String> words, Predicate<String> filter) {
         return words.stream()
                 .filter(filter)
